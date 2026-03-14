b41a598fe8016358854bf13a6ce93894ddd47944
Net2Fox <trakounet.fox@gmail.com>
2026-03-14 19:43:00 +0300

    Добавлены методы для форматирования расписания по преподавателю, аудитории

--- EJournalTelegramBot/Service/ScheduleFormatter.cs
+++ EJournalTelegramBot/Service/ScheduleFormatter.cs
@@ -6,39 +6,67 @@ namespace EJournalTelegramBot.Service;
 
 public class ScheduleFormatter
 {
-    public string Format(string groupName, Schedule? schedule)
+    private string Format(string title, Schedule? schedule, Func<ScheduleItem, string> formatItem)
     {
-        if (schedule != null)
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Расписание для *{groupName}* на *{schedule.Day:dd.MM.yyyy}*:");
-            sb.AppendLine();
-            if (schedule.ScheduleItems != null &&  schedule.ScheduleItems.Count != 0)
-            {
-                for (int i = 0; i < schedule.ScheduleItems.Count; i++)
-                {
-                    ScheduleItem sc =  schedule.ScheduleItems[i];
-                    DateTime start = DateTime.Parse(sc.StartTime);
-                    DateTime end = DateTime.Parse(sc.EndTime);
-                    if (i != 0 && int.Parse(sc.Num) != 1)
-                    {
-                        int rest = (TimeSpan.Parse(sc.StartTime) - 
-                                    TimeSpan.Parse(schedule.ScheduleItems[i - 1].EndTime)).Minutes;
-                        sb.AppendLine($"_Перерыв {rest} минут_\n");
-                    }
-                    sb.Append($"{sc.Num}.* {start:HH:mm}-{end:HH:mm}*. _{sc.Teacher}_ — ");
-                    sb.AppendLine(sc.Room == "ВМ" ? "Выездное мероприятие" : sc.Room == "ДОТ" ? "ДОТ" : $"ауд. {sc.Room}");
-                }
-            }
-            else
-            {
-                sb.AppendLine("Пар нет!");
-            }
-            return sb.ToString();
-        }
-        else
+        if (schedule is null)
         {
             return "Расписания нет!";
         }
+        
+        StringBuilder formattedSchedule = new StringBuilder();
+        formattedSchedule.AppendLine($"Расписание для *{title}* на *{schedule.Day:dd.MM.yyyy}*:");
+        formattedSchedule.AppendLine();
+
+        if (schedule.ScheduleItems.Count == 0)
+        {
+            formattedSchedule.AppendLine("Пар нет!");
+        }
+        var orderedScheduleItems = schedule.ScheduleItems
+            .OrderBy(item => int.Parse(item.Num))
+            .ToList();
+        
+        for (int i = 0; i < orderedScheduleItems.Count; i++)
+        {
+            ScheduleItem scheduleItem =  orderedScheduleItems[i];
+            DateTime start = DateTime.Parse(scheduleItem.StartTime);
+            DateTime end = DateTime.Parse(scheduleItem.EndTime);
+            
+            if (i != 0 && int.Parse(scheduleItem.Num) != 1)
+            {
+                int rest = (TimeSpan.Parse(scheduleItem.StartTime) - 
+                            TimeSpan.Parse(orderedScheduleItems[i - 1].EndTime)).Minutes;
+                formattedSchedule.AppendLine($"_Перерыв {rest} минут_\n");
+            }
+            
+            formattedSchedule.Append($"{scheduleItem.Num}.* {start:HH:mm}-{end:HH:mm}*. ");
+            formattedSchedule.AppendLine(formatItem(scheduleItem));
+        }
+        
+        return formattedSchedule.ToString();
+    }
+
+    public string FormatGroup(string group, Schedule? schedule)
+    {
+        return Format(group, schedule, item => $"_{item.Teacher}_ — {Room(item.Room)}");
+    }
+    
+    public string FormatTeacher(string teacher, Schedule? schedule)
+    {
+        return Format(teacher, schedule, item => $"_{item.SubGroup}_ — {Room(item.Room)}");
+    }
+    
+    public string FormatRoom(string room, Schedule? schedule)
+    {
+        return Format(room, schedule, item => $"_{item.Teacher}_ — {item.SubGroup}");
+    }
+    
+    private string Room(string room)
+    {
+        return room switch
+        {
+            "ВМ" => "Выездное мероприятие",
+            "ДОТ" => "ДОТ",
+            _ => $"ауд. {room}"
+        };
     }
 }
