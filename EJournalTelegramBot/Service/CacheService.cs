e0827c0520a85cf5a40773b34bc0e83314b6511e
Net2Fox <trakounet.fox@gmail.com>
2026-03-14 23:06:37 +0300

    Из сервиса кэша убраны offset

--- EJournalTelegramBot/Service/CacheService.cs
+++ EJournalTelegramBot/Service/CacheService.cs
@@ -12,9 +12,6 @@ public class CacheService(ScheduleFormatter formatter)
     
     private readonly List<string> _groupCache = new();
     
-    private int _offsetTeacher = 0;
-    private int _offsetRoom = 0;
-    
     public Schedule? GetSchedule(string groupName)
     {
         return _scheduleCache.GetValueOrDefault(groupName);
@@ -65,28 +62,16 @@ public class CacheService(ScheduleFormatter formatter)
 
     public IReadOnlyList<string> GetGroups()
     {
-        return new List<string>(_groupCache);
+        return new List<string>(_groupCache.OrderBy(g => g));
     }
     
     public IReadOnlyList<string> GetTeachers()
     {
-        return new List<string>(_teacherScheduleCache.Keys.OrderBy(t => t).ToList()[_offsetTeacher..(_offsetTeacher + 12)]);
-    }
-    
-    public IReadOnlyList<string> GetTeachers(int offset)
-    {
-        _offsetTeacher += offset;
-        return new List<string>(_teacherScheduleCache.Keys.OrderBy(t => t).ToList()[_offsetTeacher..(_offsetTeacher + 12)]);
+        return new List<string>(_teacherScheduleCache.Keys.OrderBy(t => t));
     }
     
     public IReadOnlyList<string> GetRooms()
     {
-        return new List<string>(_roomScheduleCache.Keys.OrderBy(r => r).ToList()[_offsetTeacher..(_offsetTeacher + 12)]);
-    }
-    
-    public IReadOnlyList<string> GetRooms(int offset)
-    {
-        _offsetRoom += offset;
-        return new List<string>(_roomScheduleCache.Keys.OrderBy(r => r).ToList()[_offsetRoom..(_offsetRoom + 12)]);
+        return new List<string>(_roomScheduleCache.Keys.OrderBy(r => r));
     }
 }
