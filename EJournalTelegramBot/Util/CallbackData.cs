1e3644b153cc9c526d12b2804c1ec16793fc57a1
Net2Fox <trakounet.fox@gmail.com>
2026-03-14 19:45:08 +0300

    Добавлен статический класс с константами для данных в Callback

---
+++ EJournalTelegramBot/Util/CallbackData.cs
@@ -0,0 +1,9 @@
+﻿namespace EJournalTelegramBot.Util;
+
+public static class CallbackData
+{
+    public const string MainMenu = "Back";
+    public const string TeacherSuffix = "TCH";
+    public const string RoomSuffix = "RM";
+    public const string GroupSuffix = "GRP";
+}
