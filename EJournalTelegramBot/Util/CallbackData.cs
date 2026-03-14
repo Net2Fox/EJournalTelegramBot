5a59f11605df633ca5b4dd89e4ec52a071b0a2d9
Net2Fox <trakounet.fox@gmail.com>
2026-03-14 23:06:20 +0300

    В утилиту CallbackData добавлено создание данных для Callback

--- EJournalTelegramBot/Util/CallbackData.cs
+++ EJournalTelegramBot/Util/CallbackData.cs
@@ -2,8 +2,27 @@
 
 public static class CallbackData
 {
-    public const string MainMenu = "Back";
-    public const string TeacherSuffix = "TCH";
-    public const string RoomSuffix = "RM";
-    public const string GroupSuffix = "GRP";
+    private const string Sep = ":";
+    
+    public const string MainMenuAction = "MM";
+    public const string ChooseCourseAction = "CCA";
+    public const string SelectAction = "SEL";
+    public const string PageAction = "PAGE";
+
+    public const string ChooseCoursePrefix = "CCP";
+    public const string TeacherPrefix = "TCH";
+    public const string RoomPrefix = "RM";
+    public const string GroupPrefix = "GRP";
+
+    public static string Select(string prefix, string value) => $"{prefix}{Sep}{SelectAction}{Sep}{value}";
+    
+    public static string Page(string prefix, int offset) => $"{prefix}{Sep}{PageAction}{Sep}{offset}";
+    
+    public static string Action(string action) => $"_{Sep}{action}{Sep}_";
+
+    public static (string Prefix, string Action, string Value) Parse(string callbackData)
+    {
+        var parts = callbackData.Split(Sep, 3);
+        return (parts[0], parts[1], parts[2]);
+    }
 }
