ed186bfe0236ed16d24e36ba1ebd7c24c388d2ce
Net2Fox <trakounet.fox@gmail.com>
2026-03-14 19:56:51 +0300

    Добавлен класс конфигурации SubscriptionConfiguration

---
+++ EJournalTelegramBot/Configuration/SubscriptionConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace EJournalTelegramBot.Configuration;
+
+public class SubscriptionConfiguration
+{
+    public long ChatId { get; init; }
+    public List<string>? Subscriptions { get; init; } = null!;
+}
