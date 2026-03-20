c93ec112ba661ba83db9b8f89a94f7269ef249db
Net2Fox <trakounet.fox@gmail.com>
2026-03-20 15:13:44 +0300

    Добавлена модель подписки для БД

---
+++ EJournalTelegramBot/Model/SQLite/Subscription.cs
@@ -0,0 +1,8 @@
+﻿namespace EJournalTelegramBot.Model.SQLite;
+
+public class Subscription
+{
+    public int Id { get; set; }
+    public long ChatId { get; set; }
+    public required string Group { get; set; }
+}
