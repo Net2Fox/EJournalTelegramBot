8947229861b697fd3e8c4849d2815c946ed89196
Net2Fox <trakounet.fox@gmail.com>
2026-03-20 15:13:56 +0300

    Добавлен контекст BotContext для локальной БД

---
+++ EJournalTelegramBot/Context/BotContext.cs
@@ -0,0 +1,15 @@
+﻿using EJournalTelegramBot.Model.SQLite;
+using Microsoft.EntityFrameworkCore;
+
+namespace EJournalTelegramBot.Context;
+
+public class BotContext : DbContext
+{
+    public DbSet<Subscription>  Subscriptions => Set<Subscription>();
+    public BotContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=EJournalTelegramBot.db");
+    }
+}
