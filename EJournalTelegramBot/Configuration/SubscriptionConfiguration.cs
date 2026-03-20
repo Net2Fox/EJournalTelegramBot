65631f0fb7dbb1705f10620265e1e5759df78968
Net2Fox <trakounet.fox@gmail.com>
2026-03-20 15:12:11 +0300

    Удалён класс SubscriptionConfiguration

--- EJournalTelegramBot/Configuration/SubscriptionConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EJournalTelegramBot.Configuration;
-
-public class SubscriptionConfiguration
-{
-    public long ChatId { get; init; }
-    public List<string>? Subscriptions { get; init; } = null!;
-}
